d99a07c3fb888fe0c94a04ce2fa5634726a098bd
viktoria <oborotova.vika.00@gmail.com>
2024-05-24 12:58:36 +0300

    shortcode

--- employees.php
+++ employees.php
@@ -1,5 +1,4 @@
 <?php 
-
 /* Plugin name: Работники компании */
 
 add_action( 'init', 'main' );
@@ -27,48 +26,48 @@ function main() {
         'hierarchical' => true,
     ];
     
-	register_taxonomy('department', ['staff'], $taxArgs);
+  register_taxonomy('department', ['staff'], $taxArgs);
     
     
     $labels = array(
-		'name' => 'Сотрудники',
-		'singular_name' => 'Сотрудник',
-		'add_new' => 'Добавить сотрудника',
-		'add_new_item' => 'Добавить сотрудника',
-		'edit_item' => 'Редактировать сотрудника',
-		'new_item' => 'Новый сотрудник',
-		'all_items' => 'Все Сотрудники',
-		'search_items' => 'Искать Сотрудников',
-		'not_found' =>  'Сотрудников по заданным критериям не найдено.',
-		'not_found_in_trash' => 'В корзине нет сотрудников.',
-		'menu_name' => 'Сотрудники'
-	);
+    'name' => 'Сотрудники',
+    'singular_name' => 'Сотрудник',
+    'add_new' => 'Добавить сотрудника',
+    'add_new_item' => 'Добавить сотрудника',
+    'edit_item' => 'Редактировать сотрудника',
+    'new_item' => 'Новый сотрудник',
+    'all_items' => 'Все Сотрудники',
+    'search_items' => 'Искать Сотрудников',
+    'not_found' =>  'Сотрудников по заданным критериям не найдено.',
+    'not_found_in_trash' => 'В корзине нет сотрудников.',
+    'menu_name' => 'Сотрудники'
+  );
  
-	$args = array(
-		'labels' => $labels,
-		'public' => true,
-		'has_archive' => true,
-		'menu_icon' => 'dashicons-email-alt2',
-		'menu_position' => 3,
-		'supports' => array('title','editor','author','thumbnail','trackbacks','custom-fields','comments','revisions')
-	);
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'has_archive' => true,
+    'menu_icon' => 'dashicons-email-alt2',
+    'menu_position' => 3,
+    'supports' => array('title','editor','author','thumbnail','trackbacks','custom-fields','comments','revisions')
+  );
  
-	register_post_type('staff', $args);
+  register_post_type('staff', $args);
 
-	add_action( 'add_meta_boxes', 'staff_add_custom_box' );
+  add_action( 'add_meta_boxes', 'staff_add_custom_box' );
 
 function staff_add_custom_box() {
-	$screens = [ 'staff' ];
-	foreach ( $screens as $screen ) {
-		add_meta_box(
-			'staff_metabox',           // Unique ID
-			'Карточка сотрудника',     // Box title
-			'staff_custom_box_html',   // Content callback, must be of type callable
-			$screen,                   // типы постов, для которых его подключим
+  $screens = [ 'staff' ];
+  foreach ( $screens as $screen ) {
+    add_meta_box(
+      'staff_metabox',           // Unique ID
+      'Карточка сотрудника',     // Box title
+      'staff_custom_box_html',   // Content callback, must be of type callable
+      $screen,                   // типы постов, для которых его подключим
             'normal', // расположение (normal, side, advanced)
             'high' // приоритет (default, low, high, core)
-		);
-	}
+    );
+  }
 }
 
 function staff_custom_box_html( $post ) {
@@ -76,55 +75,56 @@ function staff_custom_box_html( $post ) {
     $staff_phone = get_post_meta( $post->ID, 'phone', true );
     $staff_age = get_post_meta( $post->ID, 'age', true );
     wp_nonce_field( 'seopostsettingsupdate-' . $post->ID, '_truenonce' );
-	?>
+  ?>
     <label for="phone_box">Телефон сотрудника</label>
     <input type="text" value="<?=esc_attr($staff_phone);?>" id="phone" name="phone" class="regular-text">
     
     <label for="age_box">Возраст сотрудника</label>
     <input type="number" value="<?=esc_attr($staff_age);?>" id="age" name="age" class="regular-text">
-	<?php
+  <?php
 }
 
 function true_save_meta_staff( $post_id, $post ) {
  
-	// проверка одноразовых полей
-	if ( ! isset( $_POST[ '_truenonce' ] ) || ! wp_verify_nonce( $_POST[ '_truenonce' ], 'seopostsettingsupdate-' . $post->ID ) ) {
-		return $post_id;
-	}
+  // проверка одноразовых полей
+  if ( ! isset( $_POST[ '_truenonce' ] ) || ! wp_verify_nonce( $_POST[ '_truenonce' ], 'seopostsettingsupdate-' . $post->ID ) ) {
+    return $post_id;
+  }
  
-	// проверяем, может ли текущий юзер редактировать пост
-	$post_type = get_post_type_object( $post->post_type );
+  // проверяем, может ли текущий юзер редактировать пост
+  $post_type = get_post_type_object( $post->post_type );
  
-	if ( ! current_user_can( $post_type->cap->edit_post, $post_id ) ) {
-		return $post_id;
-	}
+  if ( ! current_user_can( $post_type->cap->edit_post, $post_id ) ) {
+    return $post_id;
+  }
  
-	// ничего не делаем для автосохранений
-	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
-		return $post_id;
-	}
+  // ничего не делаем для автосохранений
+  if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
+    return $post_id;
+  }
  
-	// проверяем тип записи
-	if( !in_array($post->post_type, array('staff')) ) {
-		return $post_id;
-	}
+  // проверяем тип записи
+  if( !in_array($post->post_type, array('staff')) ) {
+    return $post_id;
+  }
  
-	if( isset( $_POST[ 'phone' ] ) ) {
-		update_post_meta( $post_id, 'phone', sanitize_text_field( $_POST[ 'phone' ] ) );
-	} else {
-		delete_post_meta( $post_id, 'phone' );
-	}
-	if( isset( $_POST[ 'age' ] ) ) {
-		update_post_meta( $post_id, 'age', sanitize_text_field( $_POST[ 'age' ] ) );
-	} else {
-		delete_post_meta( $post_id, 'age' );
-	}
+  if( isset( $_POST[ 'phone' ] ) ) {
+    update_post_meta( $post_id, 'phone', sanitize_text_field( $_POST[ 'phone' ] ) );
+  } else {
+    delete_post_meta( $post_id, 'phone' );
+  }
+  if( isset( $_POST[ 'age' ] ) ) {
+    update_post_meta( $post_id, 'age', sanitize_text_field( $_POST[ 'age' ] ) );
+  } else {
+    delete_post_meta( $post_id, 'age' );
+  }
  
-	return $post_id;
+  return $post_id;
 }
 
 add_shortcode( 'staff_list', 'staff_list_shortcode' );
 }
+
 
 function staff_list_shortcode() {
     $args = array(
